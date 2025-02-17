acafb449679894eef95ec69a680afe3a0f69c27d
Drynkin Sergey <sergsmile29@gmail.com>
2025-02-17 18:20:05 +0200

    init m7l1

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,30 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { counterReducer } from './counterSlice';
-import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import { todoReducer } from './todoSlice';
 import { filterReducer } from './filterSlice';
 
-const persistConfig = {
-  key: 'counter-persist',
-  version: 1,
-  storage,
-  blacklist: ['step'],
-  // whitelist: ['step'],
-};
-
 export const store = configureStore({
   reducer: {
-    counter: persistReducer(persistConfig, counterReducer),
     todos: todoReducer,
     filter: filterReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
